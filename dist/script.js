26a64db50fd18481cb8a64f105ab86edf3684d23
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-04-08 11:55:40 -0500

    use console log to verify that manager is sending data to page generater script

---
+++ dist/script.js
@@ -0,0 +1,46 @@
+const Manager = require('../lib/Manager');
+
+// const resultsEl = document.querySelector('#results');
+
+const createManager = data => {
+    // const cardEl = document.createElement('div');
+    // cardEl.setAttribute('class', 'card');
+    // const cardHeaderEl = document.createElement('div');
+    // cardHeaderEl.setAttribute('class', 'card-header');
+    // cardHeaderEl.setAttribute('class', 'bg-primary');
+    // const nameEl = document.createElement('p');
+    // nameEl.setAttribute('class', 'text-light');
+    // const listEl = document.createElement('ul');
+    // listEl.setAttribute('class', 'list-group');
+    // listEl.setAttribute('class', 'list-group-flush');
+    // const idEl = document.createElement('li');
+    // idEl.setAttribute('class', 'list-group-item');
+    // const emailEl = document.createElement('li');
+    // emailEl.setAttribute('class', 'list-group-item');
+    // const officeEl = document.createElement('li');
+    // officeEl.setAttribute('class', 'list-group-item');
+
+    // emailEl.innerHTML = `Email: ${data.email}`;
+    // idEl.innerHTML = `ID: ${data.id}`;
+    // officeEl.innerHTML = `Office: ${data.office}`;
+
+    // nameEl.innerHTML = `Name: ${data.name}`;
+
+    // listEl.append(emailEl);
+    // listEl.append(idEl);
+    // listEl.append(officeEl);
+
+    // cardHeaderEl.append(nameEl);
+
+    // cardEl.append(cardHeaderEl);
+    // cardEl.append(listEl);
+
+    // resultsEl.append(cardEl);
+
+    console.log(data);
+    console.log('This is pulling data');
+}
+
+createManager();
+
+module.exports = createManager;
