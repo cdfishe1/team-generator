f081863b4e1cd9b32942be3da302eddb0824eb8e
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-04-08 10:04:07 -0500

    add needed class modules to index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Enginner = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const fs = require('fs');
 
