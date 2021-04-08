940f4cf77206ec8a71b4b1ec1ed21419e75ee2db
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-04-08 10:36:22 -0500

    Call inquirer prompts inside manager class from index.js

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
-const Employee = require('./lib/Employee');
+// const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
-const Enginner = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const inquirer = require('inquirer');
-const fs = require('fs');
+// const Enginner = require('./lib/Engineer');
+// const Intern = require('./lib/Intern');
+// const inquirer = require('inquirer');
+// const fs = require('fs');
 
+const managerQuestions = async function(){
+
+ const newManager = new Manager();
+ const createManager = await newManager.promptManager();
+
+};
+
+managerQuestions();
+
+// inquirer
+//   .prompt([
+//     {
+//       type: 'input',
+//       name: 'name',
+//       message: "What is the manager's name?",
+//     },
+//     {
+//       type: 'input',
+//       message: "What is the manager's ID number?",
+//       name: 'id',
+      
+//     },
+//     {
+//       type: 'input',
+//       message: "What is the manager's email address?",
+//       name: 'email',
+    
+//     },
+
+//     {
+//         type: 'input',
+//         message: "What is the manager's office number?",
+//         name: 'office',
+      
+//       },
+//   ])
+//   .then((data) => {
+//     console.log(data);
+//   });
