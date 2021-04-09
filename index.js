de2437f64e442ec27706effadb7711b72660acbf
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-04-09 11:11:45 -0500

    Add intern prompt. Add option to add another member.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
-// const Intern = require('./lib/Intern');
+const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 // const fs = require('fs');
 
@@ -57,12 +57,16 @@ const addTeamMember = () => {
                 type: 'list',
                 message: 'What employee would you like to add?',
                 name: 'employee',
-                choices: ['Engineer', 'Intern'],
+                choices: ['Engineer', 'Intern', "I don't wish to add another team member"],
             },
         ])
         .then((data) => {
             if(data.employee === 'Engineer') {
                 createEngineer();
+            } else if (data.employee === 'Intern') {
+                createIntern();
+            } else {
+                return;
             }
         });
 };
@@ -98,6 +102,64 @@ const createEngineer = () => {
         .then((data) => {
             let engineer = new Engineer(data.name, data.id, data.email, data.github);
             createTeam(engineer);
+            moreTeamMembers();
+            
+        });
+}
+
+const createIntern = () => {
+    inquirer
+        .prompt([
+            {
+            type: 'input',
+            name: 'name',
+            message: "What is the intern's name?",
+            },
+            {
+            type: 'input',
+            message: "What is the intern's ID number?",
+            name: 'id',
+            
+            },
+            {
+            type: 'input',
+            message: "What is the intern's email address?",
+            name: 'email',
+            
+            },
+
+            {
+                type: 'input',
+                message: "What is the intern's school?",
+                name: 'github',
+            
+            },
+        ])
+        .then((data) => {
+            let intern = new Intern(data.name, data.id, data.email, data.school);
+            createTeam(intern);
+            moreTeamMembers();
+            
+        });
+}
+
+const moreTeamMembers = () => {
+    inquirer
+        .prompt([
+            {
+            type: 'list',
+            name: 'more',
+            message: "Do you wish to add another team member?",
+            choices: ['Yes', 'No'],
+            }
+            
+        ])
+        .then((data) => {
+            if(data.more === 'Yes') {
+                addTeamMember();
+            } else {
+                return;
+            }
             
         });
 }
