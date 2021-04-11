89c71c4ed45fd33976b30c83a3831ba0c173933b
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-04-11 10:36:59 -0500

    Add icons for each type of role in the cards. Remove role appelation

--- dist/createCard.js
+++ dist/createCard.js
@@ -11,7 +11,7 @@ const createCard = teamArray => {
             cards.push(` <div class="card" style="width: 18rem; display: inline-block">
             <div class="card-header bg-primary">
               <p class='text-light'>Name: ${teamArray[i].name}</p>
-              <p class='text-light'>Role: manager</p>
+              <p class='text-light'><i class="fas fa-mug-hot" style='margin-right: 0.5vw;'></i>Manager</p>
             </div>
             <ul class="list-group list-group-flush">
               <li class="list-group-item">ID: ${teamArray[i].id}</li>
@@ -24,7 +24,7 @@ const createCard = teamArray => {
             cards.push(` <div class="card" style="width: 18rem; display: inline-block">
         <div class="card-header bg-primary">
           <p class='text-light'>Name: ${teamArray[i].name}</p>
-          <p class='text-light'>Role: engineer</p>
+          <p class='text-light'><i class="fas fa-laptop-code " style='margin-right: 0.5vw;'></i>Engineer</p>
         </div>
         <ul class="list-group list-group-flush">
           <li class="list-group-item">ID: ${teamArray[i].id}</li>
@@ -36,7 +36,7 @@ const createCard = teamArray => {
           cards.push(` <div class="card" style="width: 18rem; display: inline-block">
       <div class="card-header bg-primary">
         <p class='text-light'>Name: ${teamArray[i].name}</p>
-        <p class='text-light'>Role: intern</p>
+        <p class='text-light'><i class="fas fa-graduation-cap" style='margin-right: 0.5vw;'></i>Intern</p>
       </div>
       <ul class="list-group list-group-flush">
         <li class="list-group-item">ID: ${teamArray[i].id}</li>
@@ -57,12 +57,17 @@ const createCard = teamArray => {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-        <link rel="stylesheet" href="styles.css">
+        <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+      integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+      crossorigin="anonymous"
+    />
         <title>Development Team Roster</title>
     </head>
     <body>
         <header class="jumbotron sticky-top bg-primary py-3 text-center mb-2">
-            <h1 class='text-light'>My Team</h1>
+            <h1 class='text-light'>My Development Team</h1>
           </header>
     
         <main>
